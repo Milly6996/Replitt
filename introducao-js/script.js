cd3ca205e48cbef41e7015170a366d08acb52428
Milly6996 <kubica.santos.emily@gmail.com>
2024-10-17 11:24:29 -0300

    Add files via upload

---
+++ introducao-js/script.js
@@ -0,0 +1,7 @@
+const idade = 17
+//Crie uma variável para cada um dos 
+//dados listados:(nome, sobrenome, telefone, estaCasado= true ou false)
+//Imprima cada um dos valore com console.log(<variável>)
+
+
+console.log(idade)
