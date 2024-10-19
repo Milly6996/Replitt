21a87d4d6ecf7b83f481cd7260156479a9c0019e
Milly6996 <kubica.santos.emily@gmail.com>
2024-10-18 21:58:14 -0300

    Add files via upload

---
+++ REVISAO2TRI/JS-MOBILE/funcoes.js
@@ -0,0 +1,18 @@
+function dizeOla() {
+  alert("Olá")
+}
+function olaPessoa(nome) {
+  alert("Olá " + nome);
+}
+const nome = " João";
+const sobrenome = " Cuceto ";
+const idade = 69;
+
+function dadospessoa() {
+  const dados = nome + sobrenome + idade;
+  console.log(dados)
+
+}
+dizeOla();
+olaPessoa('Gustavo');
+dadospessoa();
---
+++ REVISAO2TRI/funcoes/funcoes01.js
@@ -0,0 +1,5 @@
+function somar (n1, n2) {
+  const rcs = n1 + n2;
+  console.log("A soma é:"  rcs); //Exibe o resultado 
+}
+somar(1, 2);
