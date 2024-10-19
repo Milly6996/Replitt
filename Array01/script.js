84036c3c2d430e11fee71050ea3e1b264b6c1942
Milly6996 <kubica.santos.emily@gmail.com>
2024-10-18 21:39:52 -0300

    Add files via upload

---
+++ Array01/script.js
@@ -0,0 +1,15 @@
+//VARIAVEIS UNITARIAS
+const nome = 'Jonas';
+const nome2 = 'Julia'
+const nome3 = 'Marcos'
+
+
+
+
+
+//VARIAVEIS DE ARRAY
+const nomes = ['Jonas', 'Julia', 'Marcos']
+console.log(nomes)
+console.log(nomes[2])
+nomes[10] = "alegria+RAIVA"
+console.log(nomes[10])
