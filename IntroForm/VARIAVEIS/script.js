93a33d411f416ae1bc9975a56d68c77691518be4
Milly6996 <kubica.santos.emily@gmail.com>
2024-10-18 21:51:52 -0300

    Add files via upload

---
+++ IntroForm/VARIAVEIS/script.js
@@ -0,0 +1,9 @@
+//# ID ou identificaor
+//. class ou classe CSS
+const comidas = document.querySelector('.comidas');
+//prompt -> pergunta ao usúario (interage)
+const comida1 = prompt('Digite sua comida nº 1:')
+//Inere o conteuo do usuario dentro do HTML
+comidas.innerText = comida1;
+
+//alert
